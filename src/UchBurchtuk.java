80cb948041db36bed268a0fe2e0ce868f1adf63a
AygulSultan <jakypbaevaaygul@gmail.com>
2023-01-29 13:23:41 +0300

    Zadanie11

---
+++ src/UchBurchtuk.java
@@ -0,0 +1,42 @@
+public class UchBurchtuk {
+    private double a;
+    private double b;
+    private double c;
+
+
+    public UchBurchtuk(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double aria(){
+        double p=(a+b+c)/2;
+        return  Math.sqrt(p*(p-a)*(p-b)*(p-c));
+
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+}
